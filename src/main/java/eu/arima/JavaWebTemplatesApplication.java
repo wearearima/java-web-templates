e665a9163dc22ab88cb3eea98e83cdaff412dfda
Iñigo Telleria <inigo@arima.eu>
2016-12-26 12:34:24 +0100

    Initial import

---
+++ src/main/java/eu/arima/JavaWebTemplatesApplication.java
@@ -0,0 +1,12 @@
+package eu.arima;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaWebTemplatesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaWebTemplatesApplication.class, args);
+	}
+}
